42fa820991dc2e42383cfc943530272dc05e5d1f
Heinz Doofenshmirtz <doof@fufeldevelompent.com>
2026-08-11 15:00:38 +0000

    Initial project scaffold

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fufeldevelompent/teleportinator
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("teleporting... still in the same place. almost perfect.")
+}
